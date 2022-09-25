6ae2a9172b14bb254a53754ce70897fb49c36b60
Jelo Dimaano <jannangelodimaano@gmail.com>
2022-09-25 22:04:24 +0800

    Add Print Statement

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Hello World")
+print("Kuya Ches panget :PPP")
+print("What")
+print("HELLO PLUTO")
+
+name = "Jelo" #str
+age = 19 #int
+average = 99.9 #float
+last_name = "Dimaano" #str
+fav_num = 10 #int
+
+print(name)
+print(age)
+
