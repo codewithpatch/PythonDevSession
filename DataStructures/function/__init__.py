9887e4b4310bf57153d5584222948485a058912b
chesterdealday <43492078+codewithpatch@users.noreply.github.com>
2022-09-29 08:05:19 +0000

    Introduction to function

---
+++ DataStructures/function/__init__.py
@@ -0,0 +1,13 @@
+def get_full_name():
+  # do something
+  pass
+
+
+def print_hello_world():
+  print("Hello World!")
+
+
+
+
+print_hello_world()
+
---
+++ DataStructures/function/function.py
@@ -0,0 +1,46 @@
+# No parameter
+def print_hello_world():
+  print("Hello World!")
+
+
+def get_full_name(first_name, middle_name, last_name):
+  full_name = f"{first_name} {middle_name} {last_name}"
+
+  # print(full_name)
+  return full_name
+
+  print(full_name)
+
+
+def print_range():
+  for num in range(0, 10):
+
+    if num == 5:
+      return num
+      
+    print(num)
+
+  return "I hit last return"
+
+print(print_range())
+
+
+
+# first_name = "Chester"
+# middle_name = "Dimaano"
+# last_name = "de Alday"
+
+# How to pass parameters in a function
+chester_full_name = get_full_name("Chester", "Dimaano", "de Alday")
+bryan_full_name = get_full_name(
+  middle_name="Calderon",
+  first_name="Bryan",
+  last_name="de Alday",
+)
+
+
+gelo_dimaano = get_full_name("Angelo", "Calalo", "Dimaano")
+# hello_world = print_hello_world()
+print(gelo_dimaano)
+
+
