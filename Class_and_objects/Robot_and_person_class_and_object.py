fb028cfa67cace420d9d41c00382ddd544a26373
Bryan de Alday <bryandealday21@gmail.com>
2022-10-09 23:53:42 +0800

    Create Dir for Class and Object

---
+++ Class_and_objects/Robot_and_person_class_and_object.py
@@ -0,0 +1,33 @@
+class Robot:
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+
+    def introduce_self(self):
+        print("My name is " + self.name)
+
+
+r1 = Robot("Tom", "red", 30)
+r2 = Robot("Jerry", "blue", 40)
+
+class Person:
+    def __init__(self, name, personality, issitting):
+        self.name = name
+        self.personality = personality
+        self.is_sitting = issitting
+
+    def sit_down(self):
+        self.is_sitting = True
+
+    def stand_up(self):
+        self.is_sitting = False
+
+p1 = Person("Alice", "aggresive", False)
+p2 = Person("Becky", "talkative", True)
+
+p1.robot_owned = r2
+p2.robot_owned = r1
+
+p1.robot_owned.introduce_self()
+
