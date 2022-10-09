f973c25319f62c7fd8373d56084e6072636f31e4
Bryan de Alday <bryandealday21@gmail.com>
2022-10-10 00:28:35 +0800

    Create Dir for codewars

--- Codewars/7_kyu_categorize_new_number.py
+++ Codewars/7_kyu_categorize_new_number.py
@@ -14,7 +14,7 @@
 def open_or_senior(data):
     output = []
     for i in data:
-        if i[0] > 55 and i[1] > 7:
+        if i[0] > 54 and i[1] > 7:
             output.append('Senior')
         else:
             output.append('Open')
