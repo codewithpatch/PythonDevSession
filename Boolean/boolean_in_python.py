7d796e965295619f65cc9bdb72f9de7868b2d751
Bryan de Alday <bryandealday21@gmail.com>
2022-10-12 22:59:00 +0800

    Create Dir for Boolean

---
+++ Boolean/boolean_in_python.py
@@ -0,0 +1,12 @@
+def c_greater_than_d_plus_e(c, d, e):
+    return c > d + e
+
+
+c_greater_than_d_plus_e(5, 1, 3)
+a = c_greater_than_d_plus_e(5, 1, 5)
+
+print(a)
+def are_u_sad(is_rainy, has_umbrella):
+    return is_rainy and not has_umbrella
+m = are_u_sad(True, True)
+print(m)
