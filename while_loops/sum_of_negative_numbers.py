724a7c19cf4edaef9c8a5313e88eb608f9cfda58
Jelo Dimaano <jannangelodimaano@gmail.com>
2022-10-19 18:35:00 +0800

    Sum the negative numbers using while loop

--- while_loops/sum_of_negative_numbers.py
+++ while_loops/sum_of_negative_numbers.py
@@ -1,12 +1,8 @@
 # Using while loop
-from typing import List
-
-list2 = [5, 4, 3, 2, 1, -1, -2, -3, -4, -5]
+list_num = [5, 4, 3, 2, 1, -1, -2 , -3, -4, -5]
 total2 = 0
-i = 0
-while 1 < 2:
-    if list2[i] >= 0:
-        continue
-    total2 -= list2[i]
-    i -= 1
+i = len(list_num) - 1
+while list_num[i] < 0:
+        total2 += list_num[i]
+        i -= 1
 print(total2)
