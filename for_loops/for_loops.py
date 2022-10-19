ac2d0915a7aadcdb2b5f83507dd797870d53aeb1
Jelo Dimaano <jannangelodimaano@gmail.com>
2022-10-19 15:34:53 +0800

    Create a for loop git status

---
+++ for_loops/for_loops.py
@@ -0,0 +1,25 @@
+a = ['apple', 'banana', 'coconut', 'durian']
+for i in a:
+    print(i)
+    print(i)
+
+# Summing all the elements
+total = 0
+b = [10, 20, 30]
+for j in b:
+    total = total + j
+print(total)
+
+c = list(range(1, 5))
+print(c)
+
+for k in range(1, 5):
+    print(k)
+
+print(list(range(1, 8)))
+
+new_total = 0
+for l in range(1, 8):
+    if l % 3 == 0:
+        new_total += l
+print(new_total)
