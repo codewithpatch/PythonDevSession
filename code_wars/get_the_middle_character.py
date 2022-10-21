73f428abeacaaad57c881e45612200564a7cfbaf
Jelo Dimaano <jannangelodimaano@gmail.com>
2022-10-22 01:03:58 +0800

    Add comment in get_the_middle_character.py

--- code_wars/get_the_middle_character.py
+++ code_wars/get_the_middle_character.py
@@ -1,6 +1,8 @@
+# 7kyu
 # You are going to be given a word.Your job is to return the middle character of the word
 # If the word's length is odd, return the middle character
 # If the word'slength is even, return the middle 2 characters
+
 
 def get_middle(s):
     word = list(s)
