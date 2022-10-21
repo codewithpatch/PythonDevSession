1b12eda120256fe3e32aaf60dac572490f1ba1c3
Jelo Dimaano <jannangelodimaano@gmail.com>
2022-10-22 01:02:58 +0800

    Add comment in stop_gninnips_my_sdrow.py

--- code_wars/stop_gninnips_my_sdrow.py
+++ code_wars/stop_gninnips_my_sdrow.py
@@ -1,3 +1,4 @@
+# 6kyu
 # Write a function that takes in a string of one or more words,
 # and returns the same string, but with all five or more letter
 # words reversed (Just like the name of this Kata). Strings passed
